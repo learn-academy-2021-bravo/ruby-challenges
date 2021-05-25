69c7f2977836b636712aa5fa215f16b24ce45e26
hexflo <hexflores87@yahoo.com>
2021-05-25 14:41:08 -0700

    left off line 8

---
+++ ruby-condtionals.rb
@@ -0,0 +1,13 @@
+# # Challenge: Rock, Paper, Scissors
+# Story: As user 1, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+
+def greeter name
+   "Hello, #{name} Type either rock, paper, or scissors"
+    
+end
+p greeter 'user 1'
+# Story: As user 2, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+
+# Story: As a user, I can see a message in the terminal saying if user 1 or user 2 won the round.
+
+# Story: As a user, I can see a message in the terminal saying if there was a tie.
