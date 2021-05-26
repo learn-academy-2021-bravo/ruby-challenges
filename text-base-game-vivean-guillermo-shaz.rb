4972094732d28ec8a09ebc6df76c75b7da6c173a
Vivean28 <vivean.la@gmail.com>
2021-05-26 13:58:02 -0700

    third method

--- text-base-game-vivean-guillermo-shaz.rb
+++ text-base-game-vivean-guillermo-shaz.rb
@@ -1,58 +1,17 @@
 p 'congardulations you will be traveling but your destinasion will depend on how you will answer the foellowing question'
 
 p 'Pick airline option one, Delta. pick option two spirit airlines'
-airline = gets.chomp
+#this is a variable that will take what the user will type in terminal
+airline = gets.chomp 
 
 # we need to make a method 
 def travel_game airline_compony
     if airline_compony.downcase == 'delta'
          puts 'Congrats you are going to Europe'
-         puts "
-            __.......__
-          ."           ".
-          :               :
-          :               :
-          `.._________..'
-               :   :
-               :   :
-               :   :
-               `...' mh
-         "
+       
     elsif airline_compony.downcase == 'spirit'
          puts 'Congrats you are going to Bahams'
-         puts "
-          ___   ____  ____    __      _   _
-          (   \ (  _ \( ___)  /  \    / \_/ \                w
-          ) - ) )   / )__)  / () \  / /   \ \                                   
-          (___/ (_)\_)(____)(__)(__)(_)     (_)            
-                                        ##########
-                                        ###################
-                                   #############,,,,.....,;;                    
-                              ############,``  ,,,,...._ ''`,.''``.,         
-          _______________________############,`   ,'__,,,,,.;:
-          ,________
-                                        ,` ,'',,`` ,.,..,.  .,   . ,.  ``,    
-                                        ,`,' ,`  ,,`      |__| ',  ', `,   `,  
-                                        ,','  ; ,,`        |__|    ',  ', `,  ', 
-                                   :.'  .`,`          |__|       ', ',  ',  ; 
-                                        :`           |__|         ;  ;    ; ;
-                                   ___,,,---....___     |__|           ; ;     ;
-
-                         __,--''                ```-|__|,___        ;;'
-          ;
-                    _,-'                          |__|     ```--,,_;:
-          .
-                    ,'                             |__|              `',,      
-                    |                             |__|                   `',   
-               _      \,                            |__|                      `, 
-          | \.      \_                                                     ,`
-          |___\       `-,,._____                                Smiley 8P,'  
-                                   `'`'`'`'----,,.,,_____..,,,,-----`'`'`'`'   
-                                   __                                         
-                                   ./ |                               |\        
-                              ./   |                                         
-                              /_____|
-         "
+        
     end
 end
 p travel_game airline
@@ -67,70 +26,7 @@ transport = gets.chomp
 def uber_limo(transport_type, airline_company)
      if transport_type.downcase == 'uber' and airline_company.downcase == 'delta'
           puts "AWESOME! You arrived at the airpot 2 hours early and you get a free meal served by Al Pucino"
-          puts "
-                         ################################                                   
-                     #####################################                                
-                  ###########################################                             
-                #################################################                         
-              #####################################################                       
-            ##########################################################                    
-          ##############################################################                  
-         ###################################################################              
-        #####################################################################             
-       ########################################################################           
-      ###########################################################################         
-    ##############################'                   `############################       
-   #################'                                      `##########################    
-   ###############'                                           `########################   
-   #############'                                              `#######################   
-  ############.'                                                &######################   
-  ###########'                                                  &######################   
- ###########3                                                   `######################   
-############3                                                     `#####################  
-############3                                                      &##################### 
-############3                                                      &##################### 
-############3                                                      &######################
-############3                                                      &######################
-###########'                                                       `######################
- ##########3                                                       &##################### 
-     #####3                                 .#######.              `####################  
-      ####3  .##############.           .###############.           `##################   
-      ####3###################.      .#####################.         ##################   
-      #########################.     ########################.        ################    
-     ####' #######'  .---. `####     ########'  .---.  ########        ##############     
-     ####3  `##' .' / (#) \ `##'      `###' .''/ (#) \  ######'        #############      
-     ####3 `--'  `. \     /.'  ;     ;      `. \     /.' `----''       ############       
-     ####3   _.' `-_ `---''.-'             `-.  `---' .' ``---''       &##########        
-      ###3          -____-'                   `- ----'    `-._         &#########         
-       ##3      _---'''      ;                       -_                &#### `.#          
-       ##3     .'                                      `.             &####   :#          
-       ##`     '                                         `-.         &####    :           
-       ###`                 .                               `       &###    ) :           
-         #3                .            .                          &###       :           
-         #3               . ;        ;   .                        &###    _  .'           
-          #3              : :            :                        &##       :             
-          #3              `.             :                        ###      .'             
-           :                .       ##-  )   .                    ##      .:              
-           :           .'    `-__-~'   `'     .                   ##     : :              
-           :          .                        `.                   ~~~;~  ;              
-           `.        '                           `.                  .'    ;              
-            :       '                              `.              .'      :              
-            `.      :        _     _                :        .    .'       :              
-             :     :   _.- ~' `-.-' `--._           :      .'    .'        :              
-              ;    :  `---._____________.-.--'      :    .'    .'                         
-              `.       `.                .'             :     .'                          
-               `.        `._          _-'               ;    ;                            
-                :           ~~-----~~'                  :  .'                             
-                `.                                     .'  :                              
-                 :           _________                 :  .'                              
-                  `.                                   : :                                
-                    `.                               .'-'                                 
-                     :                               :                                    
-                      `.                           .'                                     
-                       :`.                      .-'                                       
-                       : :         __        .'                                           
-                       :  `-------'  `------'                        dp
-     "
+         
      elsif transport_type.downcase == 'uber' and airline_company.downcase == 'spirit'
           puts 'Great! You are two hours early but you have to pay for your own meal. :['
 
@@ -143,12 +39,34 @@ def uber_limo(transport_type, airline_company)
      else
           return "You lost you get to stay home"
      end
-     
-     
-     
 end
 p uber_limo(transport,airline)
 #we're now inside the flight
  #if in delta first class free everything 
  #if in spirit youre uncomfortable and have to pay for everything
 #do you choose to have a  drink or take a nap?
+
+nap_or_drink = ""
+def in_flight airline_company 
+     if airline_company.downcase == 'delta'
+          p 'Everything is free'
+          p 'Do you want a drink or take a nap'
+          nap_or_drink = gets.chomp
+          if nap_or_drink.downcase == 'drink'
+               p 'You get to party with Cher'
+          else 
+               p 'Sweet dreams'
+          end
+     elsif airline_company.downcase == 'spirit'
+          p 'Sucks for you!'
+          p 'Do you want a drink or take a nap'
+          nap_or_drink = gets.chomp
+          if nap_or_drink.downcase == 'drink'
+               p 'Great! That will be a $100 and there is mandatory tip of 70%'
+          else 
+               p 'Okay, great! That will be $20 and we take cash venmo or cash app'
+          end
+     end
+end
+p in_flight airline
+
