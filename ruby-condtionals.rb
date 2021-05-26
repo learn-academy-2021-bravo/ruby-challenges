70b2ba0f0ff52a24775881bfab6631945d37bf1d
hexflo <hexflores87@yahoo.com>
2021-05-26 08:32:33 -0700

    init conditionals

--- ruby-condtionals.rb
+++ ruby-condtionals.rb
@@ -1,13 +1,19 @@
-# # Challenge: Rock, Paper, Scissors
 # Story: As user 1, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
 
-def greeter name
-   "Hello, #{name} Type either rock, paper, or scissors"
-    
-end
-p greeter 'user 1'
 # Story: As user 2, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
 
 # Story: As a user, I can see a message in the terminal saying if user 1 or user 2 won the round.
 
 # Story: As a user, I can see a message in the terminal saying if there was a tie.
+
+p "User One: Enter 'Rock', 'Paper', or 'Scissors'"
+user_one = gets.chomp//
+p "User two: Enter 'Rock', 'Paper', or 'Scissors'"
+user_two = gets.chomp
+if user_one.downcase == 'rock' && user_two.downcase =='paper' || user_one.downcase == 'scissors' && user_two.downcase =='rock' || user_one.downcase == 'paper' && user_two.downcase =='scissors'
+    p 'User two is the winner'
+elsif user_two.downcase == 'rock' && user_one.downcase =='paper' || user_two.downcase == 'scissors' && user_one.downcase =='rock' || user_two.downcase == 'paper' && user_one.downcase =='scissors'
+    p 'User one is the winner'
+else
+    p 'draw'
+end
