c9f7cf9ece8300b42eb733db940b8ee1b01e88c3
dgohdz <diegohmailbox@gmail.com>
2021-05-25 20:39:11 -0700

    push

--- ruby-condtionals.rb
+++ ruby-condtionals.rb
@@ -1,11 +1,11 @@
 # # Challenge: Rock, Paper, Scissors
+
 # Story: As user 1, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
 
-def greeter name
-   "Hello, #{name} Type either rock, paper, or scissors"
-    
-end
-p greeter 'user 1'
+p "User one: Enter rock, paper or scrissors"
+
+user_one = gets.chomp
+
 # Story: As user 2, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
 
 # Story: As a user, I can see a message in the terminal saying if user 1 or user 2 won the round.
